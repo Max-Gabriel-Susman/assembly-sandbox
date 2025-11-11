6ffc1e9687656a74ed4a53f8e4d63bf6f820a5b6
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-11-11 15:10:12 -0700

    got console output with assembly working

--- asem.s
+++ asem.s
@@ -1,7 +1,16 @@
 .global _start
 .intel_syntax noprefix
 
-_start:
-    mov rdi, 8
-    mov rsi, rdi
-    
+_start: # entrypoint
+    mov rax, 1 # load sys_write into register rax
+    mov rdi, 1 # load sys_write into register rdi
+    lea rsi, [howdy] # load our string identified as bark into register rsi
+    mov rdx, 7 # set length of the string, plus 2 for carriage return
+    syscall
+
+    mov rax, 60 # load sys_exit call into register rax
+    mov rdi, 69 # load exit coad into register rdi
+    syscall 
+
+howdy: 
+    .asciz "howdy\n"
